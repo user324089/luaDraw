4f49d678505e466898ced519734a31dcd945a905
user324089 <user324089@protonmail.com>
2023-09-29 15:05:54 +0200

    Changed configuration constants from constexpr to const

--- src/configureConstants.cpp
+++ src/configureConstants.cpp
@@ -1,10 +1,10 @@
-extern constexpr float arrowMoveSpeed = 500;
-extern constexpr float scrollEnlargeSpeed = 1.3f;
+extern const float arrowMoveSpeed = 500;
+extern const float scrollEnlargeSpeed = 1.3f;
 
-extern constexpr float borderColorData [4] = {0.3f,0.3f,0.3f,1};
-extern constexpr float defaultColorData [4] = {1,0,1,1};
+extern const float borderColorData [4] = {0.3f,0.3f,0.3f,1};
+extern const float defaultColorData [4] = {1,0,1,1};
 
-extern constexpr float totalPointRadius = 10;
-extern constexpr float totalLineRadius = 8;
-extern constexpr float totalCircleLineRadius = 8;
-extern constexpr float borderRadius = 5;
+extern const float totalPointRadius = 10;
+extern const float totalLineRadius = 8;
+extern const float totalCircleLineRadius = 8;
+extern const float borderRadius = 5;
