f8a8f2fd501915e934f0ec2ace8283e67c903464
user324089 <user324089@protonmail.com>
2023-09-29 14:55:56 +0200

    Revert "Removed extern from variables in configuration file"
    This reverts commit 14008d3ae87e030aaf9e965df7d182beafd1d50b.

--- src/configureConstants.cpp
+++ src/configureConstants.cpp
@@ -1,10 +1,10 @@
-constexpr float arrowMoveSpeed = 500;
-constexpr float scrollEnlargeSpeed = 1.3f;
+extern constexpr float arrowMoveSpeed = 500;
+extern constexpr float scrollEnlargeSpeed = 1.3f;
 
-constexpr float borderColorData [4] = {0.3f,0.3f,0.3f,1};
-constexpr float defaultColorData [4] = {1,0,1,1};
+extern constexpr float borderColorData [4] = {0.3f,0.3f,0.3f,1};
+extern constexpr float defaultColorData [4] = {1,0,1,1};
 
-constexpr float totalPointRadius = 10;
-constexpr float totalLineRadius = 8;
-constexpr float totalCircleLineRadius = 8;
-constexpr float borderRadius = 5;
+extern constexpr float totalPointRadius = 10;
+extern constexpr float totalLineRadius = 8;
+extern constexpr float totalCircleLineRadius = 8;
+extern constexpr float borderRadius = 5;
